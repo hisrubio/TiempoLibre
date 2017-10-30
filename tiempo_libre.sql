eb733e031a306a48b0fdb7b4e59f53f67efc35e4
Jorge Pimpinela <jorgepimpinela8@gmail.com>
2017-10-30 20:12:04 +0100

    Base de datos actualizada con la tabla actividad_categoria y cambios de ids
    Signed-off-by: Jorge Pimpinela <jorgepimpinela8@gmail.com>

--- tiempo_libre.sql
+++ tiempo_libre.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-10-2017 a las 11:53:36
--- Versión del servidor: 10.1.21-MariaDB
--- Versión de PHP: 5.6.30
+-- Tiempo de generación: 30-10-2017 a las 20:09:48
+-- Versión del servidor: 10.1.28-MariaDB
+-- Versión de PHP: 7.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -29,7 +31,6 @@ SET time_zone = "+00:00";
 CREATE TABLE `actividades` (
   `idActividad` int(11) NOT NULL,
   `titulo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `idCategoria` int(11) NOT NULL,
   `objetivos` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
   `actividad` varchar(4000) COLLATE utf8_unicode_ci NOT NULL,
   `imagen` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
@@ -41,9 +42,23 @@ CREATE TABLE `actividades` (
 -- Volcado de datos para la tabla `actividades`
 --
 
-INSERT INTO `actividades` (`idActividad`, `titulo`, `idCategoria`, `objetivos`, `actividad`, `imagen`, `puntuacion`, `idUsuario`) VALUES
-(1, 'prueba1', 0, 'que salga bien', 'hola', 'images/page-1_img3.jpg', 0, 0),
-(2, 'dos', 0, 'comer', 'see', 'images/page-1_img3.jpg', 0, 0);
+INSERT INTO `actividades` (`idActividad`, `titulo`, `objetivos`, `actividad`, `imagen`, `puntuacion`, `idUsuario`) VALUES
+(1, 'prueba1', 'que salga bien', 'hola', 'images/page-1_img3.jpg', 0, 0),
+(2, 'dos', 'comer', 'see', 'images/page-1_img3.jpg', 0, 0),
+(4, 'Prueba1', 'Objetivos1', 'Descripcion1', 'images/page-1_img3.jpg', 0, 0),
+(5, 'Prueba2', 'Objetivos2', 'Descripcion2', 'images/page-1_img3.jpg', 0, 0),
+(6, 'que buen actividad va a ser esta', 'queremos mejorar el desarrollo de los niÃ±os en sus cosas motrices y esas cosas y pasarlo muy bien y tal y a disfrutar y jajajajajajajaja pues eso que esto es una prueba', 'esto me da igual', 'images/page-1_img3.jpg', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `actividad_categoria`
+--
+
+CREATE TABLE `actividad_categoria` (
+  `idActividad` int(11) NOT NULL,
+  `idCategoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Union de las tablas actividades con categorias';
 
 -- --------------------------------------------------------
 
@@ -89,7 +104,15 @@ CREATE TABLE `usuarios` (
 -- Indices de la tabla `actividades`
 --
 ALTER TABLE `actividades`
-  ADD PRIMARY KEY (`idActividad`);
+  ADD PRIMARY KEY (`idActividad`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indices de la tabla `actividad_categoria`
+--
+ALTER TABLE `actividad_categoria`
+  ADD PRIMARY KEY (`idActividad`,`idCategoria`),
+  ADD KEY `idCategoria` (`idCategoria`);
 
 --
 -- Indices de la tabla `categorias`
@@ -117,17 +140,32 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `actividades`
 --
 ALTER TABLE `actividades`
-  MODIFY `idActividad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idActividad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
   MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
   MODIFY `idComentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `actividad_categoria`
+--
+ALTER TABLE `actividad_categoria`
+  ADD CONSTRAINT `actividad_categoria_ibfk_1` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`idCategoria`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `actividad_categoria_ibfk_2` FOREIGN KEY (`idActividad`) REFERENCES `actividades` (`idActividad`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
