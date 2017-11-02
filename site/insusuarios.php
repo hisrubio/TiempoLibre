90cec9082904158a1c74387a11fd7c7a2b47755b
Miguel <hisrubio51@gmail.com>
2017-11-02 10:15:37 +0100

    login

--- site/insusuarios.php
+++ site/insusuarios.php
@@ -8,10 +8,19 @@ $fNac=$_POST['fecNac'];
 
 include("conexion.php");
 
-$sql="INSERT INTO usuarios(usuario, nombre, correo, fecNac, password) VALUES ('$nUsu', '$nApe', '$cor', '$fNac', '$pas')";
+$sql="SELECT *
+	from usuarios
+	where usuario=\"$nUsu\"";
 
-mysqli_query($conexion, $sql) or die("Error en la consulta de insercion $sql");
+$registros=mysqli_query($conexion, $sql);
+$total=mysqli_num_rows($registros);
 
-mysqli_close($conexion);
-header("location:miCuenta.php");
+if($total==0){
+	$sql1="INSERT INTO usuarios(usuario, nombre, correo, fecNac, password) VALUES ('$nUsu', '$nApe', '$cor', '$fNac', '$pas')";
+	mysqli_query($conexion, $sql1) or die("Error en la consulta de insercion $sql1");
+	mysqli_close($conexion);
+	header("location:miCuenta.php");
+}else{
+	echo "<h2>EL USUARIO YA EXISTE</h2> Pulse <a href='miCuenta.php'>aqui</a> para continuar.";
+}
 ?>
