3b035853418db77394e293370269e7d9df2500d8
Miguel <hisrubio51@gmail.com>
2017-11-02 20:14:57 +0100

    solucion ñ, y borrado de archivos

--- site/subirActividad.php
+++ site/subirActividad.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+ <?php
+      header ('Content-type: text/html; charset=utf-8');
+    ?>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
